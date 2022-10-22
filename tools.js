7f19449f7c567937ff8a707af9210258fca6ab85
Kyytox <thomas.caillault@protonmail.com>
2022-10-22 19:45:58 +0200

    add base

--- tools.js
+++ tools.js
@@ -3,7 +3,7 @@ export const Tools = [
         site: "Font Awesome",
         url: "https://fontawesome.com/",
         description: "Icônes pour site",
-        img1: "./public/img/fontAwesome.png"
+        img1: "./img/fontAwesome.png"
     },
     {
         site: "Icons 8",
@@ -15,162 +15,162 @@ export const Tools = [
         site: "Flaticon",
         url: "https://www.flaticon.com/",
         description: "Icônes / stickers pour site",
-        img: "./public/img/flaticon.png"
+        img: "./img/flaticon.png"
     },
     {
         site: "CodePen",
         url: "https://codepen.io/",
         description: "IDE Online",
-        img: "./public/img/codepen.png"
+        img: "./img/codepen.png"
     },
     {
         site: "Collabotary",
         url: "https://colab.research.google.com/",
         description: "IDE Python Online",
-        img: "./public/img/collaboratory.png"
+        img: "./img/collaboratory.png"
     },
     {
         site: "Fonts Squirrel",
         url: "https://www.fontsquirrel.com/",
         description: "Fonts - Police caractères",
-        img: "./public/img/fontSquirrel.png"
+        img: "./img/fontSquirrel.png"
     },
     {
         site: "1001 Fonts",
         url: "https://www.1001fonts.com/",
         description: "Fonts - Police caractères",
-        img: "./public/img/1001Fonts.png"
+        img: "./img/1001Fonts.png"
     },
     {
         site: "Looka",
         url: "https://looka.com/",
         description: "Création de Logo",
-        img: "./public/img/looka.png"
+        img: "./img/looka.png"
     },
     {
         site: "Grid Cheat Sheet",
         url: "https://alialaa.github.io/css-grid-cheat-sheet/",
         description: "Guide visuel de grille CSS",
-        img: "./public/img/cssGridCheatSheets.png"
+        img: "./img/cssGridCheatSheets.png"
     },
     {
         site: "AOS",
         url: "https://michalsnik.github.io/aos/",
         description: "Animate On Scroll Library",
-        img: "./public/img/aos.png"
+        img: "./img/aos.png"
     },
     {
         site: "Autoprefixer CSS",
         url: "https://autoprefixer.github.io/",
         description: "Ajoute des préfixe a ton CSS pour les différents navigateurs",
-        img: "./public/img/autoprefixer.png"
+        img: "./img/autoprefixer.png"
     },
     {
         site: "CSS Accordion Slider Generator",
         url: "https://accordionslider.com/",
         description: "Créez des curseurs d'accordéon Responsive",
-        img: "./public/img/cssAccordionSlider.png"
+        img: "./img/cssAccordionSlider.png"
     },
     {
         site: "CSS Loaders",
         url: "https://cssloaders.github.io/",
         description: "CSS Loaders",
-        img: "./public/img/cssLoader.png"
+        img: "./img/cssLoader.png"
     },
     {
         site: "KeyFrames",
         url: "https://keyframes.app/",
         description: "CSS Animations - Colors - Shadows - Caractère Code",
-        img: "./public/img/keyFrames.png"
+        img: "./img/keyFrames.png"
     },
     {
         site: "Unsplash",
         url: "https://unsplash.com/",
         description: "Source d'images librement utilisables",
-        img: "./public/img/unsplash.png"
+        img: "./img/unsplash.png"
     },
     {
         site: "Coolors",
         url: "https://coolors.co/",
         description: "Color palette Geneartor",
-        img: "./public/img/coolors.png"
+        img: "./img/coolors.png"
     },
     {
         site: "CSS Gradient",
         url: "https://cssgradient.io/",
         description: "Gradient CSS",
-        img: "./public/img/cssGradiant.png"
+        img: "./img/cssGradiant.png"
     },
     {
         site: "10015",
         url: "https://10015.io/",
         description: "Tools Text - Image - CSS - Coding - Colrs - Social Media - Miscellaneous",
-        img: "./public/img/10015.png"
+        img: "./img/10015.png"
     },
     {
         site: "DevTooly",
         url: "https://devtooly.com/",
         description: "Frontend Developer tools",
-        img: "./public/img/devTooly.png"
+        img: "./img/devTooly.png"
     },
     {
         site: "OverAPI Developer-Tools",
         url: "https://overapi.com/developer-tools",
         description: "CSS - JS - Design",
-        img: "./public/img/overAPI.png"
+        img: "./img/overAPI.png"
     },
     {
         site: "ChartJS",
         url: "https://www.chartjs.org/",
         description: "JS Graphiques",
-        img: "./public/img/chartsJS.png"
+        img: "./img/chartsJS.png"
     },
     {
         site: "ThreeJS",
         url: "https://threejs.org/",
         description: "JS 3D Librairie",
-        img: "./public/img/threeJS.png"
+        img: "./img/threeJS.png"
     },
     {
         site: "Slick",
         url: "https://kenwheeler.github.io/slick/",
         description: "JS Caroussel",
-        img: "./public/img/slick.png"
+        img: "./img/slick.png"
     },
     {
         site: "Cleave.JS",
         url: "https://nosir.github.io/cleave.js/",
         description: "Formatez votre contenu <input/>",
-        img: "./public/img/cleaveJS.png"
+        img: "./img/cleaveJS.png"
     },
     {
         site: "ReqBin",
         url: "https://reqbin.com/",
         description: "Online REST / SOAP API testing - JS - Python - PHP - JSON - XML - Curl",
-        img: "./public/img/reqbin.png"
+        img: "./img/reqbin.png"
     },
     {
         site: "Readme",
         url: "https://readme.so/",
         description: "Créer un .Readme pour GitHub",
-        img: "./public/img/readme.png"
+        img: "./img/readme.png"
     },
     {
         site: "Chirr App",
         url: "https://getchirrapp.com/",
         description: "Ecrire des Threads Twitter",
-        img: "./public/img/chirrApp.png"
+        img: "./img/chirrApp.png"
     },
     {
         site: "Carbon",
         url: "https://carbon.now.sh/",
         description: "Partager des images de votre code source",
-        img: "./public/img/carbon.png"
+        img: "./img/carbon.png"
     },
     {
         site: "UI Buttons",
         url: "https://ui-buttons.web.app/",
         description: "Buttons HTML-CSS",
-        img: "./public/img/UIButtons.png"
+        img: "./img/UIButtons.png"
     }
 ]
