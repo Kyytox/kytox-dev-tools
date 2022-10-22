6c20d7d55e1b7782e9e4f02f3b16172b0dfe5717
Kyytox <thomas.caillault@protonmail.com>
2022-10-22 19:24:58 +0200

    display list tools

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,27 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { Tools, } from '../tools';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [tools, setTools] = useState(Tools)
+
+  const listTools = Tools.map((item) => 
+    <div className='box'>
+      <a href={item.url} target="_blank" rel="noopener noreferrer">
+        <img src={item.img}></img>      
+        <div class="overlay">
+          <p>{item.site}</p>
+          <p>{item.description}</p>
+        </div>
+      </a>
+    </div>
+);
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='list-tools'>
+        {listTools}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
