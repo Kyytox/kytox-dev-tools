4ae31bff7dc15958fcfd53448dada3d4b2150dc8
Kyytox <thomas.caillault@protonmail.com>
2022-10-22 20:46:40 +0200

    random sort list tools

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,11 @@ import { Tools, } from '../tools';
 import './App.css'
 
 function App() {
-  // const [tools, setTools] = useState(Tools)
 
-  const listTools = Tools.map((item) => 
+  console.log(Tools)
+  const sortlistTools = Tools.sort(()=> Math.random() - 0.5);
+  console.log(sortlistTools)
+  const listTools = sortlistTools.map((item) => 
     <div className='box'>
       <a href={item.url} target="_blank" rel="noopener noreferrer">
         <img src={item.img}></img>      
